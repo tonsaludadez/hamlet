56e02797e7b532d1cc1d1b4df1aa0b275484663b
tonsaludadez <tondsaludadez@gmail.com>
2016-10-13 15:29:12 +0800

    Added Modal Box

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	public void onClick(){
+		Application.Quit ();
+	}
+}
