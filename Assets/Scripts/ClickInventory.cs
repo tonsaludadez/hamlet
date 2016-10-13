44bb922346b4ee2ad1192bd1b59640a2d5705b35
Ice Quezon <icequezon@outlook.com>
2016-10-13 15:21:31 +0800

    Click Functionality

---
+++ Assets/Scripts/ClickInventory.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ClickInventory : MonoBehaviour {
+
+    Button myButton;
+    public GameObject dBox;
+    public Text title;
+
+    // Use this for initialization
+    void Awake()
+    {
+        myButton = GetComponent<Button>();
+
+        myButton.onClick.AddListener(() => { OpenInventory(); });
+    }
+
+    void OpenInventory()
+    {
+        Debug.Log(myButton.transform.parent.gameObject);
+        dBox.SetActive(true);
+        title.text = "Inventory";
+    }
+}
---
+++ Assets/Scripts/CloseDialougue.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CloseDialougue : MonoBehaviour {
+
+    Button myButton;
+
+	// Use this for initialization
+	void Awake() {
+        myButton = GetComponent<Button>();
+
+        myButton.onClick.AddListener(() => { CloseDialougeBox(); });
+	}
+
+    void CloseDialougeBox()
+    {
+        Debug.Log(myButton.transform.parent.gameObject);
+        GameObject parent = myButton.transform.parent.gameObject;
+        parent = parent.transform.parent.gameObject;
+        parent = parent.transform.parent.gameObject;
+        parent = parent.transform.parent.gameObject;
+        parent.SetActive(false);
+    }
+
+}
