c2c83bfbf249cc506f6f6a7e9a87b35c367004bf
LanceGab <LanceGab>
2016-10-20 15:02:34 +0800

    uploaded unity project

--- Assets/Scripts/structureFarm.cs
+++ Assets/Scripts/structureFarm.cs
@@ -4,30 +4,64 @@ using System.Collections;
 
 public class structureFarm : MonoBehaviour {
 
-	public static int levelFarm = 1;
-	public Canvas RMHud;
-	public Slider sliderFood;
+	public static structureFarm farm;
+	public Text textHunterList;
+
+	public Hunter[] farmerArray;
+
+	public int levelFarm;
+	//public Canvas RMHud;
+	//public Slider sliderFood;
 
 	// Use this for initialization
 
 	void Start () {
 	//	GameController.controller.setFoodData (50);
 		Debug.Log (GameController.controller.foodData);
+		levelFarm = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-		sliderFood.value = GameController.controller.foodData;
+	//	sliderFood.value = GameController.controller.foodData;
 
 	}
+
+	//singleton stuff
+	void Awake(){
+		if(farm == null)
+		{
+			DontDestroyOnLoad(gameObject);
+			farm = this;
+		}
+
+		else if (farm != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	//lovely edited this
+/*	public Hunter HunterArray (){
+		if(hunterArray.length() != 0){
+			int i = 0;
+			for(; i<hunterArray.length()-1;)
+		}
+		
+		return hunterArray;
+	}*/
+
+
 
 	void OnMouseDown() 
 	{
 		print ("clicked");
-		addFoodFromFarm(3);
-
+		//addFoodFromFarm(3);
+		//getFarmers();
 	}
+
+
 	//this function calculates the cumulative level of all hunters currently working in the farm.
 	private int countHunterLevel(){
 		// return <retrievedNumberOfHuntersWorkingInTheFarm>;
--- Assets/Scripts/structureWorkshop.cs
+++ Assets/Scripts/structureWorkshop.cs
@@ -3,13 +3,41 @@ using System.Collections;
 
 public class structureWorkshop : MonoBehaviour {
 
+	public static structureWorkshop workshop;
+
+	public int levelWorkshop = 1;
+
+	//singleton stuff
+	void Awake(){
+		if(workshop == null)
+		{
+			DontDestroyOnLoad(gameObject);
+			workshop = this;
+		}
+
+		else if (workshop != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
+
 	// Update is called once per frame
 	void Update () {
-	
+
+	}
+
+	void OnMouseDown() 
+	{
+		print ("clicked");
+	}
+
+	public int getLevel(){
+		return levelWorkshop;
 	}
 }
