94c7e00b495facc0eff4c06e973040c6f165b500
Tilmo8 <santino_gaara@yahoo.com>
2016-10-20 19:38:36 +0800

    Added HunterList and Modified Hunter

---
+++ Assets/Scripts/HunterList.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class HunterList : MonoBehaviour {
+
+	public Hunter[] hunterList;
+
+	public Hunter getID(int keyID){			//Store keys in your Building then utilize this to get the hunter
+		return new Hunter();			//I'll fix it later
+	}
+
+}
