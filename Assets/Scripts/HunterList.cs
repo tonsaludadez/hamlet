0075057ef279c8c921cb6ce307b3be4f4d6e20dd
Tilmo8 <santino_gaara@yahoo.com>
2016-10-22 23:07:25 +0800

    Modified Hunter.cs, HunterList.cs

--- Assets/Scripts/HunterList.cs
+++ Assets/Scripts/HunterList.cs
@@ -5,8 +5,8 @@ public class HunterList : MonoBehaviour {
 
 	public Hunter[] hunterList;
 
-	public Hunter getID(int keyID){			//Store keys in your Building then utilize this to get the hunter
-		return new Hunter();			//I'll fix it later
+	public Hunter getID(int keyID){		//Store keys in your Building then utilize this to get the hunter
+		return new Hunter(1);			//I'll fix it later
 	}
 
 }
