d3748d0f49d763e5377ba4cb575c98095c7a5b8d
Tilmo8 <santino_gaara@yahoo.com>
2016-10-23 17:38:23 +0800

    Modified Hunter and HunterList. Sorting can be now used.

--- Assets/Scripts/Hunter.cs
+++ Assets/Scripts/Hunter.cs
@@ -3,17 +3,17 @@ using System.Collections;
 using System;
 
 public class Hunter : MonoBehaviour {
-	public string hunterName;
-	public bool hunterGender;	//true for male, False for female. Note: Bool is equivalent to 0 & 1
+	private string hunterName;
+	public bool hunterGender;			//true for male, False for female. Note: Bool is equivalent to 0 & 1
 
-	private int hunterID; 		//unique identifier for Hunter instance, essentially a 'key'
+	public int hunterID; 				//unique identifier for Hunter instance, essentially a 'key'
 	
-	public int farmingLvl;		//Hunter's current level [Farming]
-	public int researchLvl;		//Hunter's current level [Research]
-	public int huntingLvl;		//Hunter's current level [Hunting]
+	public int farmingLvl;				//Hunter's current level [Farming]
+	public int researchLvl;				//Hunter's current level [Research]
+	public int huntingLvl;				//Hunter's current level [Hunting]
 
 	public int farmingExp;				//Hunter's current xp to NextLevel [Farming]
-	public int researchExp;				//Hunter's current xp to NextLevel [Research]
+	public int researchExp;			//Hunter's current xp to NextLevel [Research]
 	public int huntingExp;				//Hunter's current xp to NextLevel [Hunting]
 	
 	public int farmingToNextLvl;		//How many xp 'til NextLevel [Farming]
@@ -24,9 +24,8 @@ public class Hunter : MonoBehaviour {
 	public int accumulatedResearchExp;	//Total xp gained/accumulated [Research]
 	public int accumulatedHuntingExp;	//Total xp gained/accumulated [Hunting]
 
-	public string hunterTitle;			//Hunter's 'title' based on current xp levels; May not be necessary [see: title()]
+	private string hunterTitle;			//Hunter's 'title' based on current xp levels; May not be necessary [see: title()]
 
-	//public static int MAX_EXP = 100; //don't use this
 	public static int DEFAULT_LEVEL = 1;
 
 	public Hunter(int id){		//*ATTENTION* requires Random Name Generator
@@ -47,27 +46,82 @@ public class Hunter : MonoBehaviour {
 		accumulatedHuntingExp = 0;
 	}
 
-	// Use this for initialization
-	void Start () {
+	/*`````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````*/
+
+	//Properties
+
+		/*
+			Note: Use like a variable.
+				i.e. name = Hunter.HunterName	//getter
+
+			Message:
+				"I just made some in case we'd rather use properties.
+				We can remove them later in the final project"
+													-Santino
+		*/
+
+		//Hunter Name
+		public string HunterName{ get; set;}
+
+		//Skill Levels
+		public int FarmingLvl{ get; set;}
+		public int ResearchLvl{ get; set;}
+		public int HuntingLvl{ get; set;}
+
+		//Exp values::Current Level
+		public int FarmingExp{ get; set;}
+		public int ResearchExp{ get; set;}
+		public int HuntingExp{ get; set;}
 	
-	}
+		//Exp values::To Next Level
+		public int FarmingToNextLvl
+		{
+			get{
+				return farmingToNextLvl;
+			}
+		}
 
-	// Update is called once per frame
-	void Update () {
+		public int ResearchToNextLvl{
+			get{
+				return researchToNextLvl;
+			}
+		}
 
-	}
+		public int HuntingToNextLvl
+		{
+			get{
+				return huntingToNextLvl;
+			}
+		}
+
+
+		//Exp values::Total Accumulated Exp
+		public int AccumulatedFarmingExp
+		{
+			get{
+				return accumulatedFarmingExp;
+			}
+		}
+		public int AccumulatedResearchExp
+		{
+			get{
+				return accumulatedResearchExp;
+			}
+		}
+		public int AccumulatedHuntingExp
+		{
+			get{
+				return accumulatedHuntingExp;
+			}
+		}
+
+		//Hunter Title
+		public string HunterTitle{ get; set;}			//useless because of title()
+
 
 	/*`````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````*/
 
 	/*Hunter Data Funtions [Excluding Level and Serial]*/
-
-	public string getName(){				//getter function	
-		return hunterName;
-	}
-
-	public void setHunterName(string n){	//setter function
-		hunterName = n;
-	}
 
 	public string title(){					//getter function; determines title based on level comparison
 		/*
@@ -261,7 +315,54 @@ public class Hunter : MonoBehaviour {
 
 	/*Comparison Function [for sorting]*/
 
+	//Compares 'this' with another Hunter instance
+	//Comparator defines what is compared (i.e. sort by name, strength, level)
 	public int compare(Hunter operand, string comparator){
-		return new int();
+		/*
+		Comparators
+			name		=	Compares Hunter Names
+			farm		=	Compares Farming Levels
+			research	=	Compares Research Levels
+			hunt		=	Compares Hunter Levels
+			title		=	Compares Titles
+			recent		=	Compares recency (based on SerialID)
+		*/
+
+		if(comparator == "name")
+		{
+
+		}
+
+		else if(comparator == "farm"){
+			return compareValue(accumulatedFarmingExp, operand.AccumulatedFarmingExp);
+		}
+
+		else if(comparator == "research"){
+			return compareValue(accumulatedResearchExp, operand.AccumulatedResearchExp);
+		}
+
+		else if(comparator == "hunt"){
+			return compareValue(accumulatedHuntingExp, operand.AccumulatedHuntingExp);
+		}
+
+		else if(comparator == "title"){
+			return String.Compare(title(), operand.title(), false);
+		}
+
+		else if(comparator == "recent"){
+			return compareValue(serialID(), operand.serialID());
+		}
+
+		return 0;
+	}
+
+	//Helps in comparing int values; output is madeto be similar to String.Compare
+	private int compareValue(int a, int b){
+		if (a < b)
+			return -1;
+		else if (a > b)
+			return 1;
+		else
+			return 0;
 	}
 }
--- Assets/Scripts/HunterList.cs
+++ Assets/Scripts/HunterList.cs
@@ -9,4 +9,53 @@ public class HunterList : MonoBehaviour {
 		return new Hunter(1);			//I'll fix it later
 	}
 
+	/*`````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````*/
+
+	//Sorting Function
+
+	/**
+	*
+	* @author francis1
+ 	*/
+
+ 	//On first call, put 0 as lower and array.size - 1 as higher
+	private void sortBy(int lowerIndex, int higherIndex, string sortType) {
+         
+        int i = lowerIndex;
+        int j = higherIndex;
+        // calculate pivot number, I am taking pivot as middle index number
+        Hunter pivot = hunterList[lowerIndex+(higherIndex-lowerIndex)/2];
+        // Divide into two arrays
+        while (i <= j) {
+            /**
+             * In each iteration, we will identify a number from left side which 
+             * is greater then the pivot value, and also we will identify a number 
+             * from right side which is less then the pivot value. Once the search 
+             * is done, then we exchange both numbers.
+             */
+            while (hunterList[i].compare(pivot,sortType) < 0) {
+                i++;
+            }
+            while (hunterList[j].compare(pivot,sortType) > 0) {
+                j--;
+            }
+            if (i <= j) {
+                exchangeNumbers(i, j);
+                //move index to next position on both sides
+                i++;
+                j--;
+            }
+        }
+        // call quickSort() method recursively
+        if (lowerIndex < j)
+            sortBy(lowerIndex, j, sortType);
+        if (i < higherIndex)
+            sortBy(i, higherIndex, sortType);
+    }
+    private void exchangeNumbers(int i, int j) {
+        Hunter temp = hunterList[i];
+        hunterList[i] = hunterList[j];
+        hunterList[j] = temp;
+    }
+
 }
